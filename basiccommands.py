39cfdbf0877913eb3fc0e09271aec049ccd9cd53
Austin Bao <49621580+AustinBao@users.noreply.github.com>
2020-04-27 11:41:44 -0600

    Add files via upload

---
+++ basiccommands.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox(executable_path="C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Python 3.7\geckodriver.exe")
