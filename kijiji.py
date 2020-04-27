8f709e64cf2348aadac9b3186cc2544da38578c6
AustinBao <austin_dun@outlook.com>
2020-04-27 14:55:39 -0600

    huge change

--- kijiji.py
+++ kijiji.py
@@ -1,23 +1,46 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 driver = webdriver.Firefox(executable_path=r'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Python 3.7\geckodriver.exe')
-driver.maximize_window()
-driver.implicitly_wait(20)
+
 driver.get("https://www.kijiji.ca/")
+
+driver.maximize_window()
 
 element = driver.find_element_by_id("SearchKeyword")
 element.click()
-Askuser = input("What do you need to buy today?")
-element.send_keys(Askuser)
-element.send_keys(Keys.ENTER)
 
+# Using readline()
+file1 = open('keywords.txt', 'r')
+count = 0
 
-result = driver.find_element_by_class_name("title")
-driver.implicitly_wait(200000)
-result.click()
+while True:
+    count += 1
 
-title = driver.find_element_by_class_name("title-2323565163")
-print(title.text)
+    # Get next line from file
+    line = file1.readline()
+
+    # if line is empty
+    # end of file is reached
+    if not line:
+        break
+    print("Line{}: {}".format(count, line.strip()))
+
+    # askuser = input("What do you need to buy today?")
+    element.send_keys(line)
+    element.send_keys(Keys.ENTER)
+
+    wait = WebDriverWait(driver, 10)
+    wait = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "title")))
+    driver.find_element_by_class_name("title").click()
+
+    driver.find_element_by_class_name("title-2323565163")
+    print(driver.title)
+
+file1.close()
+
 
---
+++ venv/Scripts/conditional commands.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome(executable_path=r"C:\Users\chromedriver")
+
+driver.get("http://newtours.demoaut.com/")
+
+user_ele = driver.find_element_by_name("userName")
+print(user_ele.is_displayed()) #returns true/false based on find element
+print(user_ele.is_enabled())  #returns true/false
+
+password_ele = driver.find_element_by_name("password")
+print(password_ele.is_displayed())
+print(password_ele.is_enabled())
+
+user_ele.send_keys("mercury")
+password_ele.send_keys("mercury")
+
+driver.find_element_by_name("login").click()
+
+
+roundtrip_radio = driver.find_element_by_css_selector("input[value=roundtrip]")
+
+roundtrip_radio.is_selected()
