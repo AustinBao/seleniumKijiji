ff236431a7a4a7e3a852dc8ef41d01671e507744
Austin Bao <49621580+AustinBao@users.noreply.github.com>
2020-04-27 11:48:06 -0600

    Delete basiccommands.py

--- basiccommands.py
+++
@@ -1,4 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
-driver = webdriver.Firefox(executable_path="C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Python 3.7\geckodriver.exe")
