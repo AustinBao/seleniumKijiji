b68da7517ae8fd5bf59fc2eac04cf38496303c44
AustinBao <austin_dun@outlook.com>
2020-05-03 21:21:37 -0600

    file reader operators

--- FileReader.py
+++ FileReader.py
@@ -5,23 +5,41 @@ f=open("MathQuestions.txt","r")
 line1 = f.readlines()[0]
 f.close()
 
-var1 = line1[0].strip().split(' ')
-var2 = line1[4].strip().split(' ')
-operator = line1[2].strip().split(' ')
+var1 = line1[0].strip().split(" ")
+var2 = line1[4].strip().split(" ")
+operator = line1[2].strip().split(" ")
 
 int1 = [int(line1[0]) for line1 in var1]
 int2 = [int(line1[0]) for line1 in var2]
-# operator = [int(line1[0]) for line1 in var2]
+# operator = [int(line1[0]) for line1 in operator]
+
+
+if operator == '/':
+    resultDivi = int1 / int2
+    print(int1 + " " + operator + " " + int2 + " = " + resultDivi)
+
+if operator == '-':
+    resultSub = int1 - int2
+    print(int1 + " " + operator + " " + int2 + " = " + resultSub)
+
+if operator == '*':
+    resultMulti = int1 * int2
+    print(int1 + " " + operator + " " + int2 + " = " + resultMulti)
+
+if operator == '+':
+    resultAdd = int1 + int2
+    print(int1 + " " + operator + " " + int2 + " = " + resultAdd)
 
 print(int1)
 print(int2)
 print(operator)
 print(var1)
 print(var2)
+print(resultAdd)
+print(resultSub)
+print(resultMulti)
+print(resultDivi)
 
-if operator == '+':
-    result = int1 + int2
-    print(int1 ," ", operator ," ", int2 ," = ", result)
 
 f.close()
 
