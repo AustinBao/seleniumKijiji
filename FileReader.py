87deaad6031925aecb764547061319f0f2b93e66
AustinBao <austin_dun@outlook.com>
2020-05-03 20:56:23 -0600

    get variables from equation

--- FileReader.py
+++ FileReader.py
@@ -1,18 +1,49 @@
 import math
 import re
 
-f = open('MathQuestions.txt', 'r')
+f=open("MathQuestions.txt","r")
+line1 = f.readlines()[0]
+f.close()
 
-file_contents = f.read()
+var1 = line1[0].strip().split(' ')
+var2 = line1[4].strip().split(' ')
+operator = line1[2].strip().split(' ')
 
-string1 = "498results should get"
-map(int, re.findall(r'\d+', string1))
-[498]
+int1 = [int(line1[0]) for line1 in var1]
+int2 = [int(line1[0]) for line1 in var2]
+# operator = [int(line1[0]) for line1 in var2]
 
-filepath = 'MathQuestions.txt'
-with open(filepath) as fp:
-   for cnt, line in enumerate(fp):
-        print("Line {}: {}".format(cnt, line))
+print(int1)
+print(int2)
+print(operator)
+print(var1)
+print(var2)
+
+if operator == '+':
+    result = int1 + int2
+    print(int1 ," ", operator ," ", int2 ," = ", result)
 
 f.close()
 
+
+
+# line1 = f.readline()
+# print(line1)
+#
+# line2 = f.readline()
+# print(line2)
+#
+# line3 = f.readline()
+# print(line3)
+#
+# line4 = f.readline()
+# print(line4)
+#
+# f.close();
+
+
+
+# filepath = 'MathQuestions.txt'
+# with open(filepath) as fp:
+#    for cnt, line in enumerate(fp):
+#         print("Line {}: {}".format(cnt, line))
